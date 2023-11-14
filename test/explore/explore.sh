681e5074445370fe3b71d5618f7e11047e268f00
Scott Trent <trent@jp.ibm.com>
2023-11-14 15:28:18 +0900

    exploratory code

---
+++ test/explore/explore.sh
@@ -0,0 +1,8 @@
+oc apply -f explore.yaml
+echo kepler metrics via thanos querier
+oc exec -it curl-pod -n openshift-kepler-operator -- sh -c 'curl -k https://thanos-querier.openshift-monitoring.svc.cluster.local:9091/metrics -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"' 2>&1 | tee keplerMetrics.txt
+echo
+echo "susql metrics directly from susql prometheus"
+oc exec -it curl-pod -n openshift-kepler-operator -- sh -c 'curl -k http://prometheus-susql.openshift-monitoring.svc.cluster.local:9090/metrics' 2>&1| tee susqlMetrics.txt
+oc delete -f explore.yaml &
+
