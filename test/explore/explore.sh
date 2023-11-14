6a8403821980a4f63f317b91e2d634e44526c313
Scott Trent <trent@jp.ibm.com>
2023-11-14 15:51:15 +0900

    improve reliability

--- test/explore/explore.sh
+++ test/explore/explore.sh
@@ -1,8 +1,9 @@
 oc apply -f explore.yaml
+sleep 2
+
 echo kepler metrics via thanos querier
 oc exec -it curl-pod -n openshift-kepler-operator -- sh -c 'curl -k https://thanos-querier.openshift-monitoring.svc.cluster.local:9091/metrics -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"' 2>&1 | tee keplerMetrics.txt
 echo
 echo "susql metrics directly from susql prometheus"
-oc exec -it curl-pod -n openshift-kepler-operator -- sh -c 'curl -k http://prometheus-susql.openshift-monitoring.svc.cluster.local:9090/metrics' 2>&1| tee susqlMetrics.txt
+oc exec -it curl-pod -n openshift-kepler-operator -- sh -c 'curl -k http://prometheus-susql.openshift-kepler-operator.svc.cluster.local:9090/metrics' 2>&1| tee susqlMetrics.txt
 oc delete -f explore.yaml &
-
