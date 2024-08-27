c81fc176da06fb94233ec407069bcfc4d0124033
Scott Trent <trent@jp.ibm.com>
2024-08-27 19:29:35 +0900

    carbon prep
    Signed-off-by: Scott Trent <trent@jp.ibm.com>

--- cmd/main.go
+++ cmd/main.go
@@ -71,7 +71,7 @@ func main() {
 	var susqlLogLevel string = "-5"
 	// Carbon Intensity Factor in grams CO2 / Joule
 	var carbonIntensity string = "0.00000000011583333"
-	var carbonMethod string = "1"
+	var carbonMethod string = "static" // options: static, simpledynamic, sdk
 	var carbonIntensityUrl string = "https://api.electricitymap.org/v3/carbon-intensity/latest?zone=%s"
 	var carbonLocation string = "JP-TK"
 	var carbonQueryRate string = "60"
@@ -185,6 +185,7 @@ func main() {
 	}
 
 	// TODO: verify that carbonMethod is an expected value. If not log warning and set to default value.
+	// (static, simpledynamic, sdk)
 	// Note: assume that carbonIntensityUrl, carbonLocation, and carbonQueryFilter are OK. If not, we will log errors at runtime.
 
 	samplingRateInteger, err := strconv.Atoi(samplingRate)
